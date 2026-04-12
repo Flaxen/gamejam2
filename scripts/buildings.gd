5517d22fddebc17ef8ffb89ddb00849862f46505
Flaxen <alexander.carlsson@live.se>
2026-04-12 02:32:34 +0200

    added UI button overlay functionaliy for all buildings

--- scripts/buildings.gd
+++ scripts/buildings.gd
@@ -4,10 +4,6 @@ extends Node2D
 
 var scene_to_build = null
 
-func _on_button_pressed() -> void:
-	scene_to_build = building_scenes[0]
-	$ghost.region_rect = Rect2(3*64, -2, 64, 44)
-	$ghost.visible = true
 
 func _process(_delta: float) -> void:
 	
@@ -22,3 +18,38 @@ func _on_map_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> v
 			instance.global_position = get_global_mouse_position()
 			scene_to_build = null
 			$ghost.visible = false
+
+func _on_button_pressed() -> void:
+	scene_to_build = building_scenes[0]
+	$ghost.region_rect = Rect2(3*64, -2, 64, 44)
+	$ghost.visible = true
+
+func _on_button_2_pressed() -> void:
+	scene_to_build = building_scenes[0]
+	$ghost.region_rect = Rect2(6*64, -2, 64, 44)
+	$ghost.visible = true
+
+func _on_button_3_pressed() -> void:
+	scene_to_build = building_scenes[0]
+	$ghost.region_rect = Rect2(0*64, -2, 64, 44)
+	$ghost.visible = true
+
+func _on_button_4_pressed() -> void:
+	scene_to_build = building_scenes[0]
+	$ghost.region_rect = Rect2(2*64, -2, 64, 44)
+	$ghost.visible = true
+
+func _on_button_5_pressed() -> void:
+	scene_to_build = building_scenes[0]
+	$ghost.region_rect = Rect2(1*64, -2, 64, 44)
+	$ghost.visible = true
+
+func _on_button_6_pressed() -> void:
+	scene_to_build = building_scenes[0]
+	$ghost.region_rect = Rect2(4*64, -2, 64, 44)
+	$ghost.visible = true
+
+func _on_button_7_pressed() -> void:
+	scene_to_build = building_scenes[0]
+	$ghost.region_rect = Rect2(5*64, -2, 64, 44)
+	$ghost.visible = true
